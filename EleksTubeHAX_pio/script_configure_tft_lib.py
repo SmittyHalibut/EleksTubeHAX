dc56c1ec1e22e0458715362b1c5a23c10c07e5a6
Clemens Sutor <clemenssutor@gmail.com>
2024-10-08 02:20:02 +0200

    fixed partition table(s) - reduced number of partitons like in the main branch, increased the partition sizes of the rest (for 8MB version) - modified platform.ini structure and removed 8MB versions of the python scripts - modified python scripts for pre build changes, now checking for board name and adding it to the path

--- EleksTubeHAX_pio/script_configure_tft_lib.py
+++ EleksTubeHAX_pio/script_configure_tft_lib.py
@@ -1,27 +1,32 @@
-# Import("env")  # should not be required; un-comment if you have issues
-import shutil
+# In pure Python environment, the script looks broken!
+# Import from SCons.Script is already available, if the PlatformIO build environment is used to call it.
+#from SCons.Script import Import
 
-# print("Current CLI targets", COMMAND_LINE_TARGETS)
-# print("Current Build targets", BUILD_TARGETS)
+#To have access to the PIO build environment variables, we need to import the env modul from SCons.Script
+Import("env")
+
+import shutil
 
 print("===== copying TFT config files ===== ")
 
 # https://stackoverflow.com/questions/123198/how-to-copy-files
 
 # source file location: EleksTubeHAX_pio\src\GLOBAL_DEFINES.h  &  _USER_DEFINES.h
-# target file location: EleksTubeHAX_pio\.pio\libdeps\esp32dev\TFT_eSPI\User_Setup.h
+# target file location: EleksTubeHAX_pio\.pio\libdeps\esp32dev8MB\TFT_eSPI\User_Setup.h
+
+# Get the board type from the used environment
+board = env.GetProjectOption("board")
+
+# define target file and directory with the name of the board in the path
+targetDir = "./.pio/libdeps/" + board + "/TFT_eSPI"
+targetFile = targetDir + "/User_Setup.h"
 
 # copy using Python libraries
 # "copy" changes file timestamp -> lib is always recompiled.
-# "copy2" keeps file timestamp -> lib is compiled once
-ret = shutil.copy2('./src/_USER_DEFINES.h', './.pio/libdeps/esp32dev/TFT_eSPI')
+# "copy2" keeps file timestamp -> lib is compiled once (until changed define files)
+ret = shutil.copy2('./src/_USER_DEFINES.h', targetDir)
 print("Copied {ret}".format(**locals()))
-ret2 = shutil.copy2('./src/GLOBAL_DEFINES.h', './.pio/libdeps/esp32dev/TFT_eSPI/User_Setup.h')
+ret2 = shutil.copy2('./src/GLOBAL_DEFINES.h', targetFile)
 print("Copied {ret2}".format(**locals()))
-
-# copy using Windows command line
-# native "copy" command keeps file timestamp -> lib is compiled once
-# env.Execute("copy .\\src\\_USER_DEFINES.h .\\.pio\\libdeps\\esp32dev\\TFT_eSPI")
-# env.Execute("copy .\\src\\GLOBAL_DEFINES.h .\\.pio\\libdeps\\esp32dev\\TFT_eSPI\\User_Setup.h")
 
 print("Done copying TFT config files!")
